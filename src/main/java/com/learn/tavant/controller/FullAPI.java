2a80f4e627f3c201d0e4587309a3173da554451a
Anil Yadav <anil.yadav@tavant.com>
2024-01-17 17:53:16 +0530

    added header, principle, pageable objects

---
+++ src/main/java/com/learn/tavant/controller/FullAPI.java
@@ -0,0 +1,53 @@
+package com.learn.tavant.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.awt.print.Pageable;
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+public class FullAPI {
+
+
+    /*
+    @RequestHeader(HttpHeaders.CONTENT_TYPE) String contentType; --> when accepting single header
+    @RequestHeader HashMap<Object, Object> headers; --> multiple but only one value
+    @RequestHeader MultiValueMap headers; --> multiple
+    @RequestHeader HttpHeaders headers; --> headers object
+
+    Principle Object is to get user basic auth details. no need to send
+    Pageable object is used when implementing pagination
+
+    */
+
+
+    @GetMapping(path = "/full-api/{number}")
+    public ResponseEntity<Object> getResult(
+            @PathVariable int number,
+            @RequestParam String pathVariable,
+            @RequestBody List<String> list,
+            @RequestHeader HttpHeaders headers,
+            HttpServletRequest request,
+            Principal principal,
+            Pageable pageable
+    ){
+        System.out.println(headers.get("Version"));
+        System.out.println(headers.get("Accept-Content"));
+        System.out.println(headers.get("my-number"));
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+
+
+
+        //setting response headers
+        responseHeaders.add(HttpHeaders.CONTENT_ENCODING, "utf-8");
+        responseHeaders.add(HttpHeaders.CONTENT_LANGUAGE, "en");
+        responseHeaders.add(HttpHeaders.SERVER, "localhost");
+        return ResponseEntity.ok().headers(responseHeaders).body(number);
+    }
+}
