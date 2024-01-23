c421dd826cfa2289881f092f36a2dd011425fbf4
Anil Yadav <anil.yadav@tavant.com>
2024-01-23 11:09:48 +0530

    modified log method impl

--- src/main/java/com/learn/tavant/config/LogMethodImpl.java
+++ src/main/java/com/learn/tavant/config/LogMethodImpl.java
@@ -3,13 +3,12 @@ package com.learn.tavant.config;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.slf4j.Logger;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
 
 @Aspect
 @Component
